428b01ef3cb7b05cd676d91e2483c98217160407
Eric Vallsmadella Crespo <eric.vallsmadella@students.salle.url.edu>
2021-02-02 14:25:49 +0100

    FEAT : Added user registration control to avoid having two users with the same username/email.

--- src/main/java/com/codevallsma/loginTemplate/web/Controllers/RegistrationController.java
+++ src/main/java/com/codevallsma/loginTemplate/web/Controllers/RegistrationController.java
@@ -4,6 +4,7 @@ import com.codevallsma.loginTemplate.Services.UserServiceImpl;
 import com.codevallsma.loginTemplate.interfaces.UserService;
 import com.codevallsma.loginTemplate.mapper.UserMapper;
 import com.codevallsma.loginTemplate.model.User;
+import com.codevallsma.loginTemplate.repositories.UserRepository;
 import com.codevallsma.loginTemplate.web.presentation.AuthorizationRequest;
 import com.codevallsma.loginTemplate.web.presentation.RegistrationRequest;
 import org.springframework.http.HttpStatus;
@@ -12,21 +13,39 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 public class RegistrationController {
 
 	private BCryptPasswordEncoder bCryptPasswordEncoder;
-
+	private UserRepository userRepository;
 	private UserServiceImpl userServiceImpl;
 
-	public RegistrationController(UserService userService, BCryptPasswordEncoder bCryptPasswordEncoder, UserServiceImpl userServiceImpl) {
+	public RegistrationController(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder, UserServiceImpl userServiceImpl) {
 		this.bCryptPasswordEncoder = bCryptPasswordEncoder;
 		this.userServiceImpl = userServiceImpl;
+		this.userRepository = userRepository;
 	}
 	@PostMapping("/registration")
 	public ResponseEntity<User> saveUser(@RequestBody RegistrationRequest userRequest) {
 		userRequest.setPassword(bCryptPasswordEncoder.encode(userRequest.getPassword()));
+		User user = null;
+		if(userRequest.getEmail()!=null && userRequest.getUserName()!=null){
+			user = userRepository.findByUsernameOrEmail(userRequest.getEmail());
+			if(user == null){
+				user = userRepository.findByUsernameOrEmail(userRequest.getUserName());
+			}
+		} else  {
+			throw new ResponseStatusException(
+					HttpStatus.FORBIDDEN, "Some fields are empty"
+			);
+		}
+		if (user != null){
+			throw new ResponseStatusException(
+					HttpStatus.FORBIDDEN, "There's an existing user with those credentials"
+			);
+		}
 		User parsedUser = UserMapper.toRegistrationDomain(userRequest);
 		final User userToSave = userServiceImpl.saveUserWithRoles(parsedUser);
 
